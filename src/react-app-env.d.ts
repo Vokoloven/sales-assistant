66e18c9dedacd934112e6c61e3a8145249368cc4
Ruslan Volovenko <vokoloven@gmail.com>
2024-03-12 14:16:25 +0200

    fix: Removed deps
    -Removed node-sass & @types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="react-scripts" />
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+
+declare module "*.scss" {
+  const content: Record<string, string>;
+  export default content;
+}
