c353516be20b0fa063188ce5b07af1159bdd8e58
Ruslan Volovenko <vokoloven@gmail.com>
2024-03-18 16:59:18 +0200

    chore: stable

--- src/pages/UpworkFeed/Filters/ScoreFilter.tsx
+++ src/pages/UpworkFeed/Filters/ScoreFilter.tsx
@@ -1,30 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {Column, Table} from "@tanstack/react-table";
-import Select, {components} from "react-select";
-
-import Icons from "../../../components/Icons/Icons";
 
 const ScoreFilter = <T,>({column}: {column: Column<T, unknown>; table: Table<T>}) => {
-  return (
-    // <Select
-    //   components={{
-    //     IndicatorSeparator: null,
-    //     DropdownIndicator: (props) => {
-    //       return (
-    //         <components.DropdownIndicator {...props}>
-    //           <Icons.ChevronDown />
-    //         </components.DropdownIndicator>
-    //       );
-    //     },
-    //   }}
-    //   defaultValue={selectedOption}
-    //   options={options}
-    //   onChange={handleChange}
-    //   styles={selectStyles(getTheme())}
-    //   menuPlacement="top"
-    // />
-    null
-  );
+  return null;
 };
 
 export default ScoreFilter;
