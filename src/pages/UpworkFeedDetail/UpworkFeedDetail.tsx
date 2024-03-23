2f496cb8ceedc6855f2d13bc77f54af9b53bcd1d
Ruslan Volovenko <vokoloven@gmail.com>
2024-03-24 01:50:49 +0200

    chore:
    -installed react-markdown
    -added spinner for upworkfeeddetail

--- src/pages/UpworkFeedDetail/UpworkFeedDetail.tsx
+++ src/pages/UpworkFeedDetail/UpworkFeedDetail.tsx
@@ -1,13 +1,17 @@
 import {useParams} from "react-router-dom";
 
+import Spinner from "../../components/Spinner/Spinner";
 import {useGetFeedsDetailQuery} from "../../redux/api/upworkFeedsApi";
+
+import styles from "./UpworkFeedDetail.module.scss";
 
 const UpworkFeedDetail = () => {
   const {id} = useParams();
-  const {data} = useGetFeedsDetailQuery({id}, {skip: !id});
+  const {data, isLoading} = useGetFeedsDetailQuery({id}, {skip: !id});
 
   console.log(data);
 
+  if (isLoading) return <div className={styles.spinner}>Loading...{<Spinner />}</div>;
   return <div>{id}</div>;
 };
 
