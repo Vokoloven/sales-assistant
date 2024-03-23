bbd7d92337ac39881625b08faae518c1b9333249
Ruslan Volovenko <vokoloven@gmail.com>
2024-03-23 14:02:55 +0200

    chore: added virtualizer for table

--- src/pages/UpworkFeed/UpworkTable.tsx
+++ src/pages/UpworkFeed/UpworkTable.tsx
@@ -1,6 +1,8 @@
 import {Table, flexRender} from "@tanstack/react-table";
-import type {FilterMeta, Column} from "@tanstack/react-table";
+import type {FilterMeta, Column, Row} from "@tanstack/react-table";
+import {useVirtualizer} from "@tanstack/react-virtual";
 import classnames from "classnames";
+import {useRef, ElementRef} from "react";
 
 import ButtonIcon from "../../components/ButtonIcon/ButtonIcon";
 import {ButtonIconStyle} from "../../components/ButtonIcon/constants";
@@ -38,84 +40,123 @@ const handleSortIcon = (
 };
 
 const UpworkTable = <T,>({table, styles}: IProps<T>) => {
+  const {rows} = table.getRowModel();
+
+  const tableContainerRef = useRef<ElementRef<"div">>(null);
+
+  const rowVirtualizer = useVirtualizer({
+    count: rows.length,
+    estimateSize: () => 50,
+    getScrollElement: () => tableContainerRef.current,
+    measureElement:
+      typeof window !== "undefined" && navigator.userAgent.indexOf("Firefox") === -1
+        ? (element) => element?.getBoundingClientRect().height
+        : undefined,
+  });
+  const virtualRows = rowVirtualizer.getVirtualItems();
+  const totalSize = rowVirtualizer.getTotalSize();
+
   return (
-    <table className={styles.table}>
-      <thead className={styles.thead}>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr
-            key={headerGroup.id}
-            className={styles.tr}
-          >
-            {headerGroup.headers.map((header) => {
-              return (
-                <th
-                  key={header.id}
-                  className={classnames(styles.th, styles[`${header.column.columnDef?.["className"]}`])}
-                  style={{
-                    minWidth: header.column.columnDef.minSize,
-                    width: header.column.columnDef.size,
-                    flex: `${header.column.columnDef.size} 0 auto`,
-                  }}
-                >
-                  <div className={styles.cell}>
-                    <span>{flexRender(header.column.columnDef.header, header.getContext())}</span>
-                    {header.column.columnDef?.["isSorted"] && (
-                      <ButtonIcon
-                        onClick={header.column.getToggleSortingHandler()}
-                        icon={handleSortIcon(header.column.getIsSorted)}
-                        buttonIconStyle={ButtonIconStyle.Input}
-                      />
-                    )}
-                  </div>
-                  {header.column.getCanFilter() ? (
-                    <div>
-                      {header.column.columnDef?.meta &&
-                      (header.column.columnDef?.meta as ICustomFilterMeta)?.filterComponent ? (
-                        (header.column.columnDef?.meta as ICustomFilterMeta)?.filterComponent({
-                          column: header.column,
-                          table,
-                        })
-                      ) : (
-                        <Filter
-                          table={table}
-                          column={header.column}
+    <div
+      ref={tableContainerRef}
+      className={styles.containerRef}
+      style={{
+        overflow: "auto",
+        position: "relative",
+        height: 1100,
+        scrollbarWidth: "thin",
+        WebkitOverflowScrolling: "touch",
+      }}
+    >
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr
+              key={headerGroup.id}
+              className={styles.tr}
+            >
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th
+                    key={header.id}
+                    className={classnames(styles.th, styles[`${header.column.columnDef?.["className"]}`])}
+                    style={{
+                      minWidth: header.column.columnDef.minSize,
+                      width: header.column.columnDef.size,
+                      flex: `${header.column.columnDef.size} 0 auto`,
+                    }}
+                  >
+                    <div className={styles.cell}>
+                      <span>{flexRender(header.column.columnDef.header, header.getContext())}</span>
+                      {header.column.columnDef?.["isSorted"] && (
+                        <ButtonIcon
+                          onClick={header.column.getToggleSortingHandler()}
+                          icon={handleSortIcon(header.column.getIsSorted)}
+                          buttonIconStyle={ButtonIconStyle.Input}
                         />
                       )}
                     </div>
-                  ) : null}
-                </th>
-              );
-            })}
-          </tr>
-        ))}
-      </thead>
-      <tbody className={styles.tbody}>
-        {table.getRowModel().rows.map((row) => {
-          return (
-            <tr
-              key={row.id}
-              className={styles.tr}
-            >
-              {row.getVisibleCells().map((cell) => {
-                return (
-                  <td
-                    key={cell.id}
-                    className={classnames(styles.td, styles[`${cell.column.columnDef?.["className"]}`])}
-                    style={{
-                      minWidth: cell.column.columnDef.minSize,
-                      width: cell.column.columnDef.size,
-                      flex: `${cell.column.columnDef.size} 0 auto`,
-                    }}
-                  >
-                    <div className={styles.cell}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</div>
-                  </td>
+                    {header.column.getCanFilter() ? (
+                      <div>
+                        {header.column.columnDef?.meta &&
+                        (header.column.columnDef?.meta as ICustomFilterMeta)?.filterComponent ? (
+                          (header.column.columnDef?.meta as ICustomFilterMeta)?.filterComponent({
+                            column: header.column,
+                            table,
+                          })
+                        ) : (
+                          <Filter
+                            table={table}
+                            column={header.column}
+                          />
+                        )}
+                      </div>
+                    ) : null}
+                  </th>
                 );
               })}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody
+          className={styles.tbody}
+          style={{height: totalSize, position: "relative"}}
+        >
+          {virtualRows.map((virtualRow) => {
+            const row = rows[virtualRow.index] as Row<T>;
+            return (
+              <tr
+                data-index={virtualRow.index}
+                ref={(node) => rowVirtualizer.measureElement(node)}
+                key={row.id}
+                className={styles.tr}
+                style={{
+                  position: "absolute",
+                  transform: `translateY(${virtualRow.start}px)`,
+                  width: "100%",
+                }}
+              >
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td
+                      key={cell.id}
+                      className={classnames(styles.td, styles[`${cell.column.columnDef?.["className"]}`])}
+                      style={{
+                        minWidth: cell.column.columnDef.minSize,
+                        width: cell.column.columnDef.size,
+                        flex: `${cell.column.columnDef.size} 0 auto`,
+                      }}
+                    >
+                      <div className={styles.cell}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</div>
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
